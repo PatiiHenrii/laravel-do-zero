a2c390d65fb1d189f8c347efee11d911bb8b02a6
patricia.damasceno <patricia.damasceno>
2020-12-29 11:34:57 -0300

    Create static routes, controllers and views

---
+++ app/Http/Controllers/Site/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('site.contact.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function form(Request $request)
+    {
+        //
+    }
+
+}
